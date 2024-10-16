9d1a0b6b2632c037d7f92a2b0d66a53a28541dc0
Hrishi1084 <115550990+Hrishi1084@users.noreply.github.com>
2024-10-16 21:34:13 +0530

    Hosting on vercel

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { loginUser, signupUser } = require('../controllers/userController')
 const router = express.Router()
 
-router.post('/login', loginUser)
-router.post('/signup', signupUser)
+router.post('inventi-cart.vercel.app/login', loginUser)
+router.post('inventi-cart.vercel.app/signup', signupUser)
 
 module.exports = router
