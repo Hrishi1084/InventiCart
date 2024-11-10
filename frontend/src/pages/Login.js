d88171e66506695c73649ccbe0d8ae4f8898efa4
Hrishi1084 <hrishisuryawanshi1084@gmail.com>
2024-11-10 16:14:39 +0530

    css change

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,23 +10,26 @@ const Login = () => {
         await login(email, password)
     }
     return (
-        <form className='login' onSubmit={handleSubmit}>
-            <h3>Log In</h3>
-            <label>Email: </label>
-            <input
-                type='email'
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
-            <label>Password: </label>
-            <input
-                type='password'
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
-            <button disabled={isLoading}>Login</button>
-            {error && <div className='error'>{error}</div>}
-        </form>
+        <div>
+            <form className='login' onSubmit={handleSubmit}>
+                <h3>Log In</h3>
+                <label>Email: </label>
+                <input
+                    type='email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <label>Password: </label>
+                <input
+                    type='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+                <button disabled={isLoading}>Login</button>
+                {error && <div className='error'>{error}</div>}
+            </form>
+            <img src='/loginbg.png' alt='background' className='loginbg'/>
+        </div>
     )
 }
 
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,39 +1,45 @@
-import { useState } from 'react'
-import { useSignup } from '../hooks/useSignup'
+import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [accountType, setAccountType]= useState('Consumer')
-    const { signup, isLoading, error } = useSignup()
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        await signup(email, password, accountType)
-    }
-    return (
-        <form className='signup' onSubmit={handleSubmit}>
-            <h3>Signup</h3>
-            <label>Email: </label>
-            <input
-                type='email'
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-            />
-            <label>Password: </label>
-            <input
-                type='password'
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-            />
-            <label>Account Type: </label>
-            <select value={accountType} onChange={(e) => setAccountType(e.target.value)}>
-                <option value="Merchant">Merchant</option>
-                <option value="Consumer">Consumer</option>
-            </select>
-            <button disabled={isLoading}>Signup</button>
-            {error && <div className="error">{error}</div>}
-        </form>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [accountType, setAccountType] = useState("Consumer");
+  const { signup, isLoading, error } = useSignup();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(email, password, accountType);
+  };
+  return (
+    <div>
+      <form className="signup" onSubmit={handleSubmit}>
+        <h3>Signup</h3>
+        <label>Email: </label>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label>Password: </label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <label>Account Type: </label>
+        <select
+          value={accountType}
+          onChange={(e) => setAccountType(e.target.value)}
+        >
+          <option value="Merchant">Merchant</option>
+          <option value="Consumer">Consumer</option>
+        </select>
+        <button disabled={isLoading}>Signup</button>
+        {error && <div className="error">{error}</div>}
+      </form>
+      <img src="/loginbg.png" alt="background" className="loginbg" />
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
